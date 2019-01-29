48fc655897a09d941f516765ed758155e2490822
Jose Pehovaz-Diez <jose.pehovazdiez@gmail.com>
2019-01-29 02:06:14 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,3 +1,16 @@
 def dictionary
-  "hello" => "hi"
-  "to"
+	dictionary = {
+		"hello" => "hi",
+		"to" => "2",
+		"two" => "2",
+		"too" => "2",
+		"four" => "4",
+		"for" => "4",
+		"be" =>s "b",
+		"you" =>s "u",
+		"at" =>s "@", 
+		"and" =>s "&"
+	}
+end
+
+def 

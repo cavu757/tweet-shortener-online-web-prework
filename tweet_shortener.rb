ee1e0e10a2d20f34f65ab2a812d267d574090da1
Jose Pehovaz-Diez <jose.pehovazdiez@gmail.com>
2019-01-29 02:36:59 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -29,5 +29,7 @@ def word_substituter(tweet)
 end
 
 def bulk_tweet_shortener(array_tweets)
-  puts array_tweets.each {|tweet| word_substituter(tweet)}
+  array_tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
 end
